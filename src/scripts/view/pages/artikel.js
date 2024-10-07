e9e4ff22fd088e7ceaeb1f62f7269ad3244e97c7
DavidBatee <daviddbate@gmail.com>
2024-10-07 20:56:53 +0700

    menambahkan file wayang dan artikel

---
+++ src/scripts/view/pages/artikel.js
@@ -0,0 +1,162 @@
+const Artikel = {
+  async render() {
+    return `
+      <div class="main-container">
+        <img src="./walpaper.png" alt="Wallpaper Image" class="walpaper">
+        <div class="container">
+          <div class="grid-container">
+            <div class="column column1">
+              <h2>Perkembangan Wayang dari Masa ke Masa</h2>
+              <p>Wayang kulit semula berkembang di lingkungan istana
+              <span>Jadi kesenian rakyat dan menyebar ke tempat - tempat jauh</span>
+              </p>
+            </div>
+            <div class="column column2">
+              <p>Wayang telah lama menjadi identitas budaya yang melekat.
+              Wayang pada saat ini tidak hanya menjadi milik masyarakat Jawa, 
+              tapi juga menjadi identitas kebudayaan nasional. Wayang juga dianggap kesenian tradisional yang tidak mati di tengah gempuran kesenian hiburan modern. Wayang yang saat ini dipentaskan tidak lepas dari perubahan sejak awal mula wayang diciptakan. Perubahan tersebut terjadi dalam waktu yang lama, bahkan beriringan dengan jatuh bangunnya kerajaan-kerajaan di Jawa.
+              </p>
+            </div>
+            <div class="column column3">
+              <p>tapi juga menjadi identitas kebudayaan nasional. Wayang juga dianggap kesenian tradisional yang tidak mati di tengah gempuran kesenian hiburan modern. Wayang yang saat ini dipentaskan tidak lepas dari perubahan sejak awal mula wayang diciptakan. Perubahan tersebut terjadi dalam waktu yang lama, bahkan beriringan dengan jatuh bangunnya kerajaan-kerajaan di Jawa.</p>
+            </div>
+            <div class="column column4">
+              <h2>Wayang Masa Prasejarah hingga Masa Hindu-Budha</h2>
+              <p>Wayang pada awalnya adalah kesenian yang hanya berkembang di Jawa. Awalin dalam jurnal berjudul Sejarah Perkembangan dan Fungsi Wayang dalam Masyarakat menjelaskan asal mula kata wayang tak bisa dilepaskan dari kata Hyang atau sosok roh spiritual yang dipercaya oleh orang Jawa sebelum datangnya Hindu-Budha.</p>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <div class="sejarah">
+          <h2 class="tema1">Sejarah Wayang</h2>
+          <p class="judul1">Sejarah tentang wayang budaya indonesia</p>
+        </div>
+        <div class="card-container">
+          <div class="card">
+            <img src="./wayang1.png" alt="Card Image 1" class="card-image">
+            <h6>Perkembangan Wayang</h6>
+            <p class="subtitle">Wayang kulit mulai populer di Jawa, terbuat dari kulit kerbau atau kambing yang diukir halus dan 
+            pertunjukan menggunakan layar dan lampu minyak untuk menciptakan bayangan.</p>
+          </div>
+          <div class="card">
+            <img src="./wayang2.png" alt="Card Image 2" class="card-image">
+            <h6>Upacara Wayang</h6>
+            <p class="subtitle">Upacara wayang kulit juga sering kali terkait dengan berbagai upacara adat dan ritual,
+             seperti upacara kelahiran, pernikahan, dan kematian. </p>
+          </div>
+          <div class="card">
+            <img src="./wayang3.png" alt="Card Image 3" class="card-image">
+            <h6>Asal Usul Wayang</h6>
+            <p class="subtitle">Wayang memiliki sejarah yang panjang dan kaya,
+             yang diyakini telah ada sejak lebih dari seribu tahun yang lalu</p>
+          </div>
+          <div class="card">
+            <img src="./wayang4.png" alt="Card Image 4" class="card-image">
+            <h6>Jenis -  Jenis Wayang</h6>
+            <p class="subtitle">Banyak berbagai jenis wayang antara lain : Wayang Kulit, Wayang Golek , Wayang Orang, Wayang Klitik dan sebagainnya.</p>
+          </div>
+        </div>
+
+        <div class="wrapper">
+          <div class="collapsible">
+            <input type="checkbox" id="collapsible-toggle-1" class="collapsible-toggle" />
+            <label for="collapsible-toggle-1">Sejarah Wayang</label>
+            <div class="collapsible-text">
+              <p class="short-text">Wayang merupakan pertunjukan wayang kulit klasik Jawa yang diketahui berkembang sejak sebelum abad ke-10. Wayang terkenal dengan pertunjukannya yang rumit dan diatur dan bentuk cerita kuno ini berasal dari pulau Jawa di Indonesia. Istilah pewayangan, berasal dari kata Indonesia untuk "bayangan". Wayang kulit dengan menggunakan figur yang terbuat dari kulit kerbau, dianggap sebagai bentuk wayang tertua yang berdiri sendiri, <span class="ellipsis">... </span><span class="read-more">Read More</span></p>
+              <p class="full-text">Wayang merupakan pertunjukan wayang kulit klasik Jawa yang diketahui berkembang sejak sebelum abad ke-10. Wayang terkenal dengan pertunjukannya yang rumit dan diatur dan bentuk cerita kuno ini berasal dari pulau Jawa di Indonesia. Istilah pewayangan, berasal dari kata Indonesia untuk "bayangan". Wayang kulit dengan menggunakan figur yang terbuat dari kulit kerbau, dianggap sebagai bentuk wayang tertua yang berdiri sendiri, referensi paling awal untuk wayang jenis itu berasal dari tahun 800-an. Perkembangan seni ini secara luas terjadi selama periode Hindu-Buddha, terutama antara 800 dan 1500. Menurut mitos, seorang pangeran bernama Aji Saka membawa aspek budaya India ke Jawa. Ritual panjang pembukaan pertunjukan wayang untuk merayakan kedatangannya di pulau itu. Yang mana, Aji Saka datang dengan membawa hanacaraka, abjad Jawa Sansekerta, yang kemudian dia bagi menjadi empat, menyebar seperempat ke masing-masing dari empat arah dan dengan demikian mentransmisikan melek huruf dan kemakmuran ke seluruh negeri.<span class="read-less"> Read Less</span></p>
+            </div>
+          </div>
+
+          <div class="collapsible">
+            <input type="checkbox" id="collapsible-toggle-2" class="collapsible-toggle" />
+            <label for="collapsible-toggle-2">Asal Usul Wayang</label>
+            <div class="collapsible-text">
+              <p class="short-text">Asal usul kata "wayang" berasal dari bahasa Jawa yang berarti bayangan, menggambarkan sifat-sifat manusia seperti marah, dengki, cinta kasih, dan sabar. Sifat tokoh-tokoh wayang mencerminkan kehidupan manusia<span class="ellipsis">... </span><span class="read-more">Read More</span></p>
+              <p class="full-text">Asal usul kata "wayang" berasal dari bahasa Jawa yang berarti bayangan, menggambarkan sifat-sifat manusia seperti marah, dengki, cinta kasih, dan sabar. Sifat tokoh-tokoh wayang mencerminkan kehidupan manusia. Ada juga pendapat yang mengaitkan kata "wayang" dengan frasa "Ma Hyang," yang berarti menuju yang maha tinggi atau Tuhan yang maha kuasa. Meskipun asal usul wayang kulit belum memiliki bukti kongkrit, beberapa pendapat menyatakan bahwa wayang kulit sudah ada sebelum abad pertama Masehi, bertepatan dengan munculnya ajaran Hindu-Buddha di Asia Tenggara. Bertolak dari asal usul sejarah keberadaan kesenian wayang kulit, karya seni wayang kulit merupakan seni dekoratif yang dapat dijadikan sebagai sarana media pendidikan, media informasi, dan juga sebagai media hiburan. Wayang sebagai media pendidikan, karena bila dilihat dari segi isinya banyak memberikan ajaran-ajaran, nasihat-nasihat, maupun contoh budi pekerti kepada manusia. Wayang sebagai media informasi, dapat dipakai untuk memberikan informasi atau dapat dipakai untuk memahami sesuatu tradisi, maupun untuk mengadakan pendekatan kepada masyarakat. Wayang sebagai media hiburan, maksudnya dapat berfungsi sebagai hiburan masyarakat atau dipakai sebagai pertunjukan untuk berbagai macam keperluan sebagai hiburan.<span class="read-less"> Read Less</span></p>
+            </div>
+          </div>
+
+          <div class="collapsible">
+            <input type="checkbox" id="collapsible-toggle-3" class="collapsible-toggle" />
+            <label for="collapsible-toggle-3">Jenis-Jenis Wayang</label>
+            <div class="collapsible-text">
+              <p class="short-text">1. Wayang Purwa<br/>Ini dianggap sebagai gaya tertua dan gaya wayang paling populer yang banyak digunakan. Pegangan wayang utama yang secara tradisional terbuat dari tanduk kerbau berada di tengah dan memegang seluruh bagian wayang mulai dari kaki, pinggang, dada, <span class="ellipsis">... </span><span class="read-more">Read More</span></p>
+              <p class="full-text">1. Wayang Purwa<br/>Ini dianggap sebagai gaya tertua dan gaya wayang paling populer yang banyak digunakan. Pegangan wayang utama yang secara tradisional terbuat dari tanduk kerbau berada di tengah dan memegang seluruh bagian wayang mulai dari kaki, pinggang, dada, hingga kepala dan rambutnya.<br/>2. Wayang Parwa<br/>Wayang parwa adalah wayang dari Bali. Wayang jenis ini biasanya dibawakan dengan menggunakan 'blencong', cahaya tertentu yang berasal dari perunggu yang diisi dengan lilin minyak kelapa.<br/>3. Wayang Betawi<br/>Wayang Betawi adalah gaya tertentu yang populer di kalangan masyarakat dan budaya Betawi. Budaya Betawi berakar di Batavia atau ibu kota Indonesia saat ini Jakarta.<br/>4. Wayang Sasak<br/>Gaya wayang ini berasal dari Nusa Tenggara Barat, di bagian timur Indonesia. Gaya Wayang ini dikembangkan agar portabel bersama dengan alat musik yang disederhanakan.<br/>5. Wayang Kancil<br/>Wayang tidak selalu menampilkan cerita tentang bangsawan dan dewa. Ada juga gaya yang hanya menampilkan cerita yang berhubungan dengan hewan. Yang paling terkenal adalah kancil ini, yang dikenal suka mencuri mentimun.<span class="read-less"> Read Less</span></p>
+            </div>
+          </div>
+
+          <div class="collapsible">
+            <input type="checkbox" id="collapsible-toggle-4" class="collapsible-toggle" />
+            <label for="collapsible-toggle-4">Tokoh-tokoh Wayang</label>
+            <div class="collapsible-text">
+              <p class="short-text">1. Semar:<br/>Semar adalah tokoh Punakawan yang sangat dihormati dalam dunia wayang. Dia dianggap sebagai jelmaan dewa, sering digambarkan dengan wajah yang sederhana dan berpenampilan humoris. Semar memiliki peran penting sebagai penasihat bijak dan pelindung para Pandawa. Ia dikenal karena kebijaksanaannya, kerendahan hati, <span class="ellipsis">... </span><span class="read-more">Read More</span></p>
+              <p class="full-text">1. Semar:<br/>Semar adalah tokoh Punakawan yang sangat dihormati dalam dunia wayang. Dia dianggap sebagai jelmaan dewa, sering digambarkan dengan wajah yang sederhana dan berpenampilan humoris. Semar memiliki peran penting sebagai penasihat bijak dan pelindung para Pandawa. Ia dikenal karena kebijaksanaannya, kerendahan hati, dan kemampuan untuk menenangkan situasi yang sulit dengan humornya.<br/>2. Gareng:<br/>Gareng adalah salah satu Punakawan yang setia menemani Semar. Dia memiliki penampilan yang lucu dengan kaki pincang dan mata juling. Gareng dikenal karena kepolosannya dan sering menjadi sumber humor dalam cerita. Meskipun demikian, ia juga bijak dan sering memberikan nasihat penting kepada para pahlawan.<br/>3. Petruk:<br/>Petruk, anak dari Semar, adalah Punakawan lain yang memiliki tubuh tinggi dan hidung panjang. Dia sering bertindak sebagai penghibur dengan lelucon dan tingkah lakunya yang konyol. Petruk juga memiliki sisi bijak dan setia, membantu para Pandawa dalam berbagai situasi.<br/>4. Bagong:<br/>Bagong adalah tokoh Punakawan yang lain, dikenal dengan tubuhnya yang gemuk dan wajah yang lucu. Dia sering memberikan komentar yang jujur dan blak-blakan. Bagong adalah simbol dari keberanian dan ketegasan dalam berbicara.<br/>5. Yudhistira:<br/>Yudhistira adalah anak tertua dari Pandawa Lima, juga dikenal sebagai Puntadewa. Dia adalah raja yang adil dan bijaksana, sering digambarkan dengan sifat sabar, jujur, dan penuh tanggung jawab. Yudhistira adalah simbol kejujuran dan moralitas dalam cerita wayang.<span class="read-less"> Read Less</span></p>
+            </div>
+          </div>
+        </div>
+                
+        <div class="sejarah-museum">
+            <h2 class="tema">Sejarah Museum Wayang</h2>
+            <p class="content">Melansir dari situs resmi Dinas Kebudayaan DKI Jakarta, Museum Wayang mulanya merupakan gereja yang didirikan oleh kelompok kolonial Belanda VOC pada tahun 1640. Gedung ini kemudian diberi nama “de oude Hollandsche Kerk". Pada tahun 1733 gereja tersebut mengalami perbaikan, dan namanya dirubah menjadi “de nieuwe Hollandsche Kerk". Gedung ini kemudian beralih fungsi menjadi gudang milik perusahaan Geo Wehry & Co tahun 1912, sebelum kemudian pada 14 Agustus 1936 ditetapkan menjadi monumen. Kemudian gedung ini dibeli oleh Bataviaasch Genootschap van Kunsten en Wetenschappen yaitu lembaga yang menangani penelitian dalam bidang seni dan ilmu pengetahuan. Sebelum menjadi museum wayang, gedung ini juga pernah dijadikan museum dengan nama "de oude Bataviasche Museum" atau museum Batavia Lama pada tahun 1937. Selanjutnya pada tahun 1957 diserahkan kepada Lembaga Kebudayaan Indonesia (LKI) dan sejak itu nama museum diganti menjadi Museum Jakarta Lama. LKI kemudian menyerahkan gedung ini kepada Departemen Pendidikan dan Kebudayaan dan pada akhirnya pada tanggal 23 Juni 1968 oleh Dirjen Kebudayaan Departemen Pendidikan dan Kebudayaan, gedung museum diserahkan kepada Pemerintah DKI Jakarta.</p>
+          </div>
+
+        <div class="second-row">
+          <div class="column column5">
+            <h2>Alat Musik Pada Wayang</h2>
+            <p>Kesenian Wayang Kulit saat ini semakin rendah peminatnya oleh karena di masa sekarang anak muda lebih memilih hiburan yang ada di gawai atau hiburan lain berbasis teknologi. Pada era ini, sebagai para generasi penerus bangsa, kita harus tetap melestarikan wayang kulit.</p>
+          </div>
+          <div class="column column6">
+            <img src="./musik.png" alt="Second Image">
+          </div>
+          <div class="column column7">
+            <p>Wayang selalu diiringi dengan alunan musik tradisional Jawa. alat musik biasanya digunakan untuk mengiringi pagelaran wayang kulit adalah alat musik gamelan, yang terdiri dari Kendang, Gender, Bonang, Slentem, Rebab, Demung, Kenong, Kempul, Suling, dan gong. Dalang yang bertugas sebagai pemimpin gamelan juga membawa pemukul kayu yang disebut Cempala untuk memberi aba-aba kapan musik dimainkan dan musik seperti apa yang harus dimainkan.</p>
+          </div>
+        </div>
+
+        
+      </div>
+      
+    `;
+  },
+
+  async afterRender() {
+    const readMoreButtons = document.querySelectorAll('.read-more');
+    const readLessButtons = document.querySelectorAll('.read-less');
+
+    readMoreButtons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        const parentParagraph = event.target.closest('.collapsible-text');
+        const shortText = parentParagraph.querySelector('.short-text');
+        const fullText = parentParagraph.querySelector('.full-text');
+        const ellipsis = parentParagraph.querySelector('.ellipsis');
+        const readLessButton = parentParagraph.querySelector('.read-less');
+
+        shortText.style.display = 'none';
+        fullText.style.display = 'block';
+        ellipsis.style.display = 'none';
+        event.target.style.display = 'none';
+        readLessButton.style.display = 'inline';
+      });
+    });
+
+    readLessButtons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        const parentParagraph = event.target.closest('.collapsible-text');
+        const shortText = parentParagraph.querySelector('.short-text');
+        const fullText = parentParagraph.querySelector('.full-text');
+        const ellipsis = parentParagraph.querySelector('.ellipsis');
+        const readMoreButton = parentParagraph.querySelector('.read-more');
+
+        shortText.style.display = 'block';
+        fullText.style.display = 'none';
+        ellipsis.style.display = 'inline';
+        event.target.style.display = 'none';
+        readMoreButton.style.display = 'inline';
+      });
+    });
+  }
+};
+
+export default Artikel;
